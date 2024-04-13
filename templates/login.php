8509a60176ac901c72dfb3e410f72286edc2b110
Mourinha08 <afonsomoura485@gmail.com>
2024-04-13 15:41:31 +0100

    updating

--- templates/login.php
+++ templates/login.php
@@ -14,8 +14,8 @@
     </button>
     <p>JUNTA-TE A NÓS E VENDE ROUPA EM SEGUNDA MÃO SEM PAGAR TAXAS!</p>
     <form id="loginForm" action="../actions/login.php" method="POST">
-        <input type="email" id="email" name="email" placeholder="Email"><br>
-        <input type="password" id="password" name="password" placeholder="Password"><br>
+        <input type="email" id="email" name="email" placeholder="Email" required><br>
+        <input type="password" id="password" name="password" placeholder="Password" required><br>
         <button type="submit" id="loginBtn">Login</button>
     </form>
     <p>Não tens uma conta? <a href="#" id="createAccountLink">Cria uma</a>.</p>
