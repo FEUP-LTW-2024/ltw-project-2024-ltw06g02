8f3362ab43a7313f478ccfde8f59968c7326edf8
Rodrigo Miranda <rucamir2004@gmail.com>
2024-04-15 16:35:35 +0100

    new function php to user and styling profile

--- database/articles.php
+++ database/articles.php
@@ -7,4 +7,14 @@
       $articles = $stmt->fetchAll();
       return $articles;
    }
-?>
+
+   function getUserArticles($db, $userID){
+      $stmt = $db->prepare(
+         "SELECT product.*, users.avatar FROM product LEFT JOIN users ON product.userID = users.userID WHERE product.userID = :userID"
+      );
+      $stmt->bindParam(':userID', $userID);
+      $stmt->execute();
+      $userArticles = $stmt->fetchAll();
+      return $userArticles;
+   }
+?> 
