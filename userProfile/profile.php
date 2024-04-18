3f3a2675129fce2d9d585b97d3a6d620431f247d
Rodrigo Miranda <rucamir2004@gmail.com>
2024-04-18 14:10:50 +0100

    profile appearing

--- userProfile/profile.php
+++ userProfile/profile.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 // Incluir arquivos de conexão com o banco de dados e classe de usuário
 require_once '../database/connection.php';
 require_once '../database/User.php';
+require_once '../templates/article.tl.php';
 
 // Obtém a conexão com o banco de dados
 $db = getDatabaseConnection();
 
 // ID do usuário específico que você quer buscar
-$specificUserId = 5; // Substitua pelo ID do usuário desejado
+$specificUserId = 1; // Substitua pelo ID do usuário desejado
 
 // Query para obter os dados do usuário específico
 $query = "SELECT * FROM users WHERE userID = :userId";
@@ -23,13 +24,7 @@ function displayUserItems(PDO $db, int $userId)
     $stmt = $db->prepare("SELECT * FROM product WHERE userID = :userId");
     $stmt->execute(['userId' => $userId]);
     $userItems = $stmt->fetchAll();
-
-    foreach ($userItems as $item) {
-        echo '<div class="item">';
-        echo '<img src="' . $item['images'] . '" alt="' . $item['name'] . '" />';
-        echo '<p>Preço: $' . $item['price'] . '</p>';
-        echo '</div>';
-    }
+    printArticleSection($userItems);
 }
 ?>
 
@@ -49,15 +44,17 @@ function displayUserItems(PDO $db, int $userId)
             <img src="<?php echo $user['avatar']; ?>" alt="Foto de Perfil do Usuário" />
         </div>
         <div class="profile-info">
-            <img src="<?php echo $user['avatar']; ?>" alt="Avatar do Usuário" />
             <h1><?php echo $user['username']; ?></h1>
             <p>Email: <?php echo $user['email']; ?></p>
             <!-- Adicione outros detalhes do perfil do usuário conforme necessário -->
             <button>Editar Perfil</button>
         </div>
     </div>
+
+    <!-- Adiciona espaço entre as informações do usuário e os itens à venda -->
+    <div class="separator"></div>
+
     <div class="item-grid">
-        <h2>Produtos à Venda</h2>
         <?php
         // Exibir os itens do usuário
         displayUserItems($db, $specificUserId);
